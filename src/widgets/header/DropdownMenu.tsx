d44a0e82c9e2ec27ba2db7981c4d4a714be0b23f
Ibragim <ibragardwich@gmail.com>
2026-01-14 19:58:43 +0300

    feat(header): DropdownMenu has been moved to a separate component, the main part of the markup is ready

---
+++ src/widgets/header/DropdownMenu.tsx
@@ -0,0 +1,47 @@
+import { forwardRef } from "react";
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+
+type NavItem = {
+  link: string;
+  text: string;
+};
+
+interface DropdownMenuProps {
+  isOpen: boolean;
+  categories: NavItem[];
+  //   listRef: RefObject<HTMLUListElement>; // RefObject - пишется когда мы принимаем реф в пропсы
+}
+
+// forwardRef - позволяет родительскому компоненту передать ref этому компоненту
+// первый параметр - тип рефа (HTMLUListElement (тип элемента, на который ссылается ref) ),
+// второй - тип пропсов (DropdownMenuProps)
+const DropdownMenu = forwardRef<HTMLUListElement, DropdownMenuProps>(
+  // деструктурируем пропсы, listRef - второй параметр forwardRef, который приходит от родителя
+  ({ isOpen, categories }, listRef) => {
+    // если dropdown закрыт, не рендерим ничего
+    if (!isOpen) return null;
+
+    return (
+      <ul
+        className={styles.header__categories_list}
+        ref={listRef}
+        id="categories-list"
+      >
+        {categories.map((category, categoryIndex) => (
+          <li key={categoryIndex} className={styles.header__categories_item}>
+            <Link
+              to={category.link}
+              className={styles.header__categories_link}
+              aria-label={`Перейти к странице ${category.text}`}
+            >
+              {category.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+);
+
+export default DropdownMenu;
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,8 +1,143 @@
+import { useEffect, useRef, useState, type FC } from "react";
+import styles from "./Header.module.scss";
+import Arrow from "@/shared/assets/icons/header/arrow-down.svg?react";
+import Search from "@/shared/assets/icons/header/search.svg?react";
+import TransitionWB from "@/shared/assets/icons/header/transition-wb.svg?react";
+import Logo from "@/shared/assets/icons/Logo.svg?react";
+import { Link } from "react-router-dom";
+import DropdownMenu from "./DropdownMenu";
 
-const Header = () => {
+const Header: FC = () => {
+  const [isCategoriesOpen, setIsCategoriesOpen] = useState(false);
+
+  const listRef = useRef<HTMLUListElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  type NavItem = {
+    link: string;
+    text: string;
+  };
+
+  const categories: NavItem[] = [
+    { link: "/women", text: "Женщинам" },
+    { link: "/men", text: "Мужчинам" },
+    { link: "/children", text: "Детям" },
+  ];
+
+  const navItems: NavItem[] = [
+    { link: "/", text: "Главная" },
+    { link: "/about", text: "О нас" },
+  ];
+
+  useEffect(() => {
+    if (!isCategoriesOpen) return;
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setIsCategoriesOpen(false);
+      }
+    };
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        listRef.current &&
+        !listRef.current.contains(event.target as Node) && // если был клик вне списка
+        divRef.current &&
+        !divRef.current.contains(event.target as Node) // если был клик вне кнопки
+      ) {
+        setIsCategoriesOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside); // слушаем клики по всему документу
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside); // функция очистки, выполнится при размонтировании или изменении isCategoriesOpen
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isCategoriesOpen]);
+
+  useEffect(() => {
+    if (isCategoriesOpen) {
+      // когда dropdown открывается, фокусируемся на первой категории
+      const firstLink = listRef.current?.querySelector(
+        "a"
+      ) as HTMLAnchorElement;
+      if (firstLink) {
+        firstLink.focus();
+      }
+    }
+  }, [isCategoriesOpen]);
+
   return (
+    <header className={`${styles.header} container`}>
+      <nav className={styles.header__nav}  role="navigation">
+        <div className={styles.header__logo_wrapper}>
+          <Link to="/" aria-label="Вернуться на главную страницу">
+            <Logo className={styles.header__logo_logo} />
+            <p className={styles.header__logo_text}>TaSaHa</p>
+          </Link>
+        </div>
+        <ul className={styles.header__list}>
+          {navItems.map((item, index) => (
+            <li key={index} className={styles.header__list_item}>
+              <Link
+                to={item.link}
+                className={styles.header__list_link}
+                aria-label={`Перейти к странице ${item.text}`}
+                onClick={(e) => {
+                  if (item.text === "Главная") e.preventDefault();
+                }} // для предотвращения навигации при toggle
+              >
+                {item.text === "Главная" ? (
+                  <div
+                    className={styles.header__expandable_wrapper}
+                    ref={divRef}
+                  >
+                    <button
+                      className={styles.header__expandable}
+                      onClick={() => setIsCategoriesOpen(!isCategoriesOpen)}
+                      aria-expanded={isCategoriesOpen}
+                      aria-controls="categories-list"
+                      aria-haspopup="menu" // указывает, что кнопка открывает меню
+                    >
+                      {item.text}
+                      <Arrow
+                        className={`${styles.header__expandable_icon} ${
+                          isCategoriesOpen
+                            ? styles.header__expandable_icon_open
+                            : ""
+                        } `}
+                      />
+                    </button>
+                    <DropdownMenu ref={listRef} isOpen={isCategoriesOpen} categories={categories}/>
+                  </div>
+                ) : (
+                  item.text
+                )}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className={styles.header__wb_search_wrapper}>
+          <Search
+            className={styles.header__search}
+            aria-label="Иконка поиска"
+          />
+          <a
+            href="https://www.wildberries.ru/brands/310895408-tasaha"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.header__wb}
+          >
+            <TransitionWB
+              className={styles.header__wb_icon}
+              aria-label="Иконка перехода на Wildberries"
+            />
+            <p className={styles.header__wb_text}>Перейти на WB</p>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
