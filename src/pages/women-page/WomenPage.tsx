9803be1a0566a412b97b47c5e2551974597df227
Ibragim <ibragardwich@gmail.com>
2026-01-26 15:40:08 +0300

    Reapply "Reapply "feat(next, api wb, RTK Query): Next isn't fully implemented yet. The WB API calculates the discount under the hood incorrectly. I'll have to enter it myself in Sanity, I guess""
    This reverts commit a63be6c206f36a8179b78ae2e156d06f27815674.

--- src/pages/women-page/WomenPage.tsx
+++ src/pages/women-page/WomenPage.tsx
@@ -3,15 +3,17 @@ import styles from "./WomenPage.module.scss";
 import { useAppDispatch } from "@/redux/store";
 import { setSlides } from "@/redux/slider/slice";
 import Slider from "@/features/slider/Slider";
-import { useGetProductsQuery } from "@/sanity/productsApi";
+
 import Spiner from "@/components/Spiner/Spiner";
+import { useGetSliderProductsQuery } from "@/sanity/productsApi";
 
 const WomenPage: FC = () => {
   const dispatch = useAppDispatch();
 
-  // получаем продукты с Sanity
-  const { data: slides, isLoading, error } = useGetProductsQuery();
+  // Получаем слайды через RTK Query (Sanity + WB фото)
+  const { data: slides, isLoading, error } = useGetSliderProductsQuery();
 
+  // Сохраняем слайды в Redux для Slider
   useEffect(() => {
     if (!isLoading && slides) {
       dispatch(setSlides(slides));
@@ -21,19 +23,12 @@ const WomenPage: FC = () => {
   if (isLoading) return <Spiner />;
   if (error) return <div>Ошибка загрузки товаров</div>;
 
+
   return (
     <div className={styles.container}>
       <Slider />
-<p>.</p>
-<p>.</p>
-<p>.</p>
-<p>.</p>
-<p>.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
+
+
     </div>
   );
 };
