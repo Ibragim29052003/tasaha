a63be6c206f36a8179b78ae2e156d06f27815674
Ibragim <ibragardwich@gmail.com>
2026-01-26 14:44:38 +0300

    Revert "Reapply "feat(next, api wb, RTK Query): Next isn't fully implemented yet. The WB API calculates the discount under the hood incorrectly. I'll have to enter it myself in Sanity, I guess""
    This reverts commit 4d4ceb04d0d981a9b43e263f1eb766d7bc71058b.

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000", // Next.js dev-сервер
-        changeOrigin: true,
-      },
-    },
-  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
