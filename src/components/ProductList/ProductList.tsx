4884fddecaf4f166230303c38bf1c0ee0c0927c9
Ibragim <ibragardwich@gmail.com>
2026-01-30 08:28:17 +0300

    feat(ProductList): finally, the product list has been added, the sanity issue has been resolved, everything works on the women's page, all that's left is to work on the styles

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,65 @@
+import type { Slide } from "@/redux/slider/types";
+import styles from "./ProductList.module.scss";
+import type { FC } from "react";
+import Spiner from "../Spiner/Spiner";
+import ProductCard from "../ProductCard/ProductCard";
+
+interface ProductListProps {
+  products: Omit<Slide, "description" | "link">[]; // берем все поля кроме описания и ссылки (она будет на детальной странице)
+  loading: boolean; // флаг загрузки
+}
+
+const ProductList: FC<ProductListProps> = ({ products, loading }) => {
+  if (loading) {
+    return(
+    <div
+        className={styles.productList__loading}
+        role="status"    
+        aria-live="polite"
+        aria-busy="true"
+        >
+        <div aria-hidden='true'>
+            <Spiner />
+        </div>
+        <p className={styles.productList__loading_text}>
+          Загрузка товаров...
+        </p>
+    </div>
+    ) 
+  }
+
+  if (products.length === 0) {
+    return (
+      <div className={styles.productList__empty} role="region" aria-live="polite">
+        <p className={styles.productList__empty_title}>Товары не найдены</p>
+        <p className={styles.productList__empty_text}>Попробуйте изменить фильтры</p>
+      </div>
+    )
+  }
+
+  return (
+    <div 
+    className={styles.productList}
+    role="region"
+    aria-label="Список товаров"
+    >
+      <ul
+        className={styles.productList__grid}
+      >
+         {products.map((product) => (
+          <li
+            key={product.id}
+            className={styles.productList__item}
+          >
+            <ProductCard product={product} />
+          </li>
+        ))}
+
+      </ul>
+
+    </div>
+  )
+
+};
+
+export default ProductList;
--- src/pages/women-page/WomenPage.tsx
+++ src/pages/women-page/WomenPage.tsx
@@ -1,39 +1,50 @@
 import { useEffect, type FC } from "react";
 import styles from "./WomenPage.module.scss";
-import { useAppDispatch } from "@/redux/store";
+import { useAppDispatch, useAppSelector } from "@/redux/store";
 import { setSlides } from "@/redux/slider/slice";
 import Slider from "@/features/slider/Slider";
-import { useGetProductsQuery } from "@/sanity/productsApi";
+import {
+  useGetFilteredProductsQuery,
+  useGetProductsQuery,
+} from "@/sanity/productsApi";
 import Spiner from "@/components/Spiner/Spiner";
+import ProductList from "@/components/ProductList/ProductList";
+import FilterPanel from "@/components/FilterPanel/FilterPanel";
+import { selectFilters } from "@/redux/filter/selectors";
 
 const WomenPage: FC = () => {
   const dispatch = useAppDispatch();
 
+  // получаем данные из редакса
+  const filters = useAppSelector(selectFilters);
+
+  const {
+    data: products,
+    isLoading: productsLoading,
+    error: productsError,
+  } = useGetFilteredProductsQuery({ ...filters, category: "women" });
+
   // получаем продукты с Sanity
-  const { data: slides, isLoading, error } = useGetProductsQuery("women");
+  const {
+    data: slides,
+    isLoading: slidesLoading,
+    error: slidesError,
+  } = useGetProductsQuery("women");
 
   useEffect(() => {
-    if (!isLoading && slides) {
+    if (!slidesLoading && slides) {
       dispatch(setSlides(slides));
     }
-  }, [dispatch, slides, isLoading]);
+  }, [dispatch, slides, slidesLoading]);
 
-  if (isLoading) return <Spiner />;
-  if (error) return <div>Ошибка загрузки товаров</div>;
+  if (slidesLoading) return <Spiner />;
+  if (slidesError || productsError) return <div>Ошибка загрузки товаров</div>;
 
   return (
     <div className={styles.container}>
       <Slider />
-<p>.</p>
-<p>.</p>
-<p>.</p>
-<p>.</p>
-<p>.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ad omnis voluptatem dignissimos modi. Accusamus doloribus quisquam nostrum facere eveniet consectetur obcaecati quas iusto reprehenderit culpa quae aspernatur, inventore architecto.</p>
+      <FilterPanel category="women" />
+      <ProductList products={products || []} loading={productsLoading} />
     </div>
   );
 };
